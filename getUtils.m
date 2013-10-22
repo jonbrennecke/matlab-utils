69909277803cfeba7a21ba320c3eea2d94f780ef
Jon Brennecke <jpbrennecke@gmail.com>
2013-10-22 12:55:35 -0700

    Update getUtils.m

--- getUtils.m
+++ getUtils.m
@@ -4,10 +4,23 @@
 function funs = getUtils
     funs.hexavigesimal = @hexavigesimal;
     funs.alphabetical_cast = @alphabetical_cast;
+    funs.numerical_cast = @numerical_cast;
 end
 
-% conversion to hexavigesimal (base 26)
-function string = hexavigesimal(number)
+% Conversion to hexavigesimal (base 26)
+% Note: what this actually returns is 'bijective base 26', 
+% or 'base 26 without a zero'. I.e., using the digits from 'A'
+% to 'Z' to represent 1-26 with no zero.
+function result = hexavigesimal(in)
+    if strcmp(class(in),'double')
+        result = hexavigesimal_cast(in);
+    elseif ischar(in)
+        result = hexavigesimal_reverse(in);
+    end
+end
+
+% Conversion from double to hexavigesimal (base 26)
+function string = hexavigesimal_cast(number)
     number = number - 1;
     if number < 0
         warning('number must be greater than 0.');
@@ -32,7 +45,22 @@ function string = hexavigesimal(number)
     end
 end
 
-% convert numbers 1-26 to chars
+% Conversion from hexavigesimal (base 26) to double
+function number = hexavigesimal_reverse(string)
+    number = numerical_cast(string(1)) * 26;
+    for i=2:length(string)-1
+        number = number * 26;
+        number = number + (numerical_cast(string(i)) * 26);
+    end
+    number = number + numerical_cast(string(end));
+end
+
+% Convert numbers 0-25 to chars
 function c = alphabetical_cast(number)
     c = char(97+str2num(sprintf('%i',number)));
+end
+
+% Convert chars to numbers 0-25
+function number = numerical_cast(c)
+    number = str2num(sprintf('%i',c)) - 97;
 end
