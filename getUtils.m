5f77a8e1db0a0f520e4888f2cc32f2f6fbfb258d
Jon Brennecke <jpbrennecke@gmail.com>
2013-11-18 14:36:23 -0800

    Update getUtils.m

--- getUtils.m
+++ getUtils.m
@@ -53,6 +53,7 @@ function operators = getOperators
     operators.arraysplit = @arraysplit;
     operators.index = @index;
     operators.strip = @strip;
+    % operators.reshape = @reshape; TODO
     % operators.hashmap = @hashmap; TODO
     operators.downsample = @downsample;
     operators.filter = @filter;
@@ -173,6 +174,14 @@ end
 % return the operating system submodule
 function os = getOS
     os.path = getPath;
+    os.open = @open_file;
+end
+
+% open a file and return its contents as char arrays delimited by newlines
+function text = open_file(filename)
+    fid = fopen(filename);
+    content = fread(fid,'*char');
+    text = split(content,'\n');
 end
 
 % ---------------------------------------- PATH ----------------------------------------
