1e790a77d9666c33c7a8393db141e155e7cc8c6b
Jon Brennecke <brennecke.jonathan@wsu.edu>
2014-02-03 12:14:41 -0800

    working of DateTime object

--- DateTime.m
+++ DateTime.m
@@ -2,15 +2,43 @@
 % DateTime object
 %
 % by @jonbrennecke / https://github.com/jonbrennecke
-%
+%`
 % Released under the MIT license (see the accompanying LICENSE.txt)
 %  
 classdef DateTime
 
-	properties (Hidden)
+	properties
+		
+		year;
+		month;
+		day;
+		hour;
+		minute;
+		micro;
+		milli;
+
+	end
+
+	properties (Hidden, SetAccess = private, GetAccess = private)
+
+		% Format Regular Expressions
+
+		% Y/M/D,H:M:S AM|PM
+		f1 = [
+			'(?<month>0[1-9]|1[0-2])(.*?)' ... 				% numbers 01-12
+			'(?<day>0[1-9]|1[0-9]|2[0-9]|3[0-1])(.*?)' ...	% numbers 01-31
+			'(?<year>\d{4}|\d{2})(.*?)' ...					% group of 4 numbers | group of 2 numbers
+			'(?<hour>0[1-9]|1[0-9]|2[0-4])(.*?)' ...		% numbers 01-24
+			'(?<min>\d{2}|\d{1})(.*?)' ...					% group of 2 numbers | 1 number [0-9]
+			'(?<sec>\d{2}|\d{1})(.*?)'...					% group of 2 numbers | 1 number [0-9]
+			'(?<meridian>AM|PM)'	...						% AM | PM
+		];
+
 	end
 
 	properties (SetAccess = private)
+
+		format;
 
 	end
 	
@@ -20,17 +48,22 @@ classdef DateTime
 	methods
 		
 		% Constructor
-		function this = DateTime
+		function this = DateTime(str)
+			if( exist('str') ) 
 
+			% 	% regexp(str,this.ISO,'names')
+
+
+			end
 		end
 
+		function setFormat(fstr)
+			
+		end
 
 	end % methods
 
 	methods (Static)
-
-
-
 	end % static methods
 
 end % XL
