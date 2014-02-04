86567d8d9cd406d929a64a208c8076da4d3e7b89
Jon Brennecke <brennecke.jonathan@wsu.edu>
2014-02-04 09:32:42 -0800

    working on DateTime

--- DateTime.m
+++ DateTime.m
@@ -4,9 +4,14 @@
 % by @jonbrennecke / https://github.com/jonbrennecke
 %`
 % Released under the MIT license (see the accompanying LICENSE.txt)
+%
+% Modelled after the 'DateTime' class in PHP 5
+% @see / http://www.php.net/manual/en/class.datetime.php
+% 
 %  
 classdef DateTime
 
+	% public properties
 	properties
 		
 		year;
@@ -19,26 +24,60 @@ classdef DateTime
 
 	end
 
-	properties (Hidden, SetAccess = private, GetAccess = private)
+	% private properties
+	properties (SetAccess = private)
 
-		% Format Regular Expressions
-
-		% Y/M/D,H:M:S AM|PM
-		f1 = [
-			'(?<month>0[1-9]|1[0-2])(.*?)' ... 				% numbers 01-12
-			'(?<day>0[1-9]|1[0-9]|2[0-9]|3[0-1])(.*?)' ...	% numbers 01-31
-			'(?<year>\d{4}|\d{2})(.*?)' ...					% group of 4 numbers | group of 2 numbers
-			'(?<hour>0[1-9]|1[0-9]|2[0-4])(.*?)' ...		% numbers 01-24
-			'(?<min>\d{2}|\d{1})(.*?)' ...					% group of 2 numbers | 1 number [0-9]
-			'(?<sec>\d{2}|\d{1})(.*?)'...					% group of 2 numbers | 1 number [0-9]
-			'(?<meridian>AM|PM)'	...						% AM | PM
-		];
+		current_format;
 
 	end
 
-	properties (SetAccess = private)
+	% Constants
+	properties (Constant = true, Hidden, SetAccess = private, GetAccess = private)
 
-		format;
+		% Regular Expression base patterns
+
+		month__ = '(?<month>0[1-9]|1[0-2])';			% numbers 01-12
+		day__ = '(?<day>0[1-9]|1[0-9]|2[0-9]|3[0-1])';	% numbers 01-31
+		year__ = '(?<year>\d{4}|\d{2})';				% group of 4 numbers | group of 2 numbers
+		hour__ = '(?<hour>0[1-9]|1[0-9]|2[0-4])';		% numbers 01-24
+		min__ = '(?<min>\d{2}|\d{1})';					% group of 2 numbers | 1 number [0-9]
+		sec__ = '(?<sec>\d{2}|\d{1})';					% group of 2 numbers | 1 number [0-9]
+		meridian__ ='(?<meridian>AM|am|PM|pm)'; 		% AM | PM
+
+		% Regular Expressions constants for various DateTime formattings
+
+		% M/D/Y | M-D-Y | M\D\Y | M D Y
+		DEFAULT_DATE = [ ...
+			DateTime.month__ '(\/|\\|\-|\s+?)' ...
+			DateTime.day__ '(\/|\\|\-|\s+?)' ...
+			DateTime.year__  ...
+		];
+
+		% H:M:S
+		DEFAULT_TIME = [ ...
+			DateTime.hour__  '(\:)' ...
+			DateTime.min__   '(\:)' ...
+			DateTime.sec__ ...
+		];
+
+		DEFAULT = [ ...
+			DateTime.DEFAULT_DATE '(.*?)' ...
+			DateTime.DEFAULT_TIME '(.*?)' ...	
+			DateTime.meridian__ ...
+		];
+
+		% TODO implement the following (from PHP's DateTime spec)
+		% DATE_ATOM; "Y-m-d\TH:i:sP"
+		% DATE_COOKIE; "l, d-M-y H:i:s T"
+		% DATE_ISO8601; "Y-m-d\TH:i:sO"
+		% DATE_RFC822; "D, d M y H:i:s O"
+		% DATE_RFC850; "l, d-M-y H:i:s T"
+		% DATE_RFC1036; "D, d M y H:i:s O"
+		% DATE_RFC1123; "D, d M Y H:i:s O"
+		% DATE_RFC2822; "D, d M Y H:i:s O"
+		% DATE_RFC3339; "Y-m-d\TH:i:sP"
+		% DATE_RSS; "D, d M Y H:i:s O"
+		% DATE_W3C; "Y-m-d\TH:i:sP" 
 
 	end
 	
@@ -48,11 +87,10 @@ classdef DateTime
 	methods
 		
 		% Constructor
-		function this = DateTime(str)
+		function this = DateTime(str,fmt)
 			if( exist('str') ) 
 
-			% 	% regexp(str,this.ISO,'names')
-
+				disp( regexp(str,this.DEFAULT,'names') )
 
 			end
 		end
